ac1c2bedbc48247e6039e10b5208b8a93446cb3c
D. Orlowski <do34spm@gmail.com>
2019-12-20 13:26:00 +0100

    removed I from Adar in not leap years

--- example/index.js
+++ example/index.js
@@ -8,4 +8,13 @@ console.log(hebrewDate(2016, 10, 2));
 
 var october = 9;
 console.log(hebrewDate(new Date(2016, october, 3)));
+
+var match = 2;
+
+console.log(hebrewDate(new Date(2019, match, 3)));
+
+console.log(hebrewDate(new Date(2020, match, 3)));
+
+var april = 3;
+console.log(hebrewDate(new Date(2020, april, 3)));
 // { year: 5777, month: 1, date: 1, month_name: 'Tishri' }
